80b5da9ea80df07f1c978d8f0db7556eb74afe78
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2022-07-13 09:21:40 +0700

    feat: add functionality to mark task as in progress

--- todo.sh
+++ todo.sh
@@ -1,31 +1,48 @@
 #/bin/bash
 
-todofile=""
+todofile="/home/fatt/.todo"
+doingfile="/home/fatt/.doing"
 
 # DMENU INTERFACE 
 function add()
 {
-	echo "" | dmenu -b -i -p "Add task: " | tr -d '\n' |  xargs -0 bash $0 add
+	echo "" | dmenu -i -fn "FontAwesome" -p  -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' | tr -d '\n' |  xargs -0 bash $0 add
 }
 
 function display()
 {
-	 bash $0 | dmenu -b -l $lines | while read task; do \
-		echo -e "Yes\nNo" | dmenu -i -b -p "Delete?" | while read opt; do \
-			[ "$opt" == "Yes" ] && delete "$task"; \
-				done; done
+	 bash $0 | dmenu -fn "FontAwesome" -p  -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -l $lines | while read task; do \
+         echo -en "Delete\nMark as doing" | dmenu -i -fn "FontAwesome" -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -p "Action" | while read action; do \
+             if [[ $action == "Delete" ]]; then delete "$task"; elif [[ $action == "Mark as doing" ]]; then doing "$task"; else echo "huh"; fi; \
+             done; done
+
+		# echo -e "Yes\nNo" | dmenu -i -fn "FontAwesome" -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -p "Delete?" | while read opt; do \
+		# 	[ "$opt" == "Yes" ] && delete "$task"; \
+		# 		done; done
+}
+
+function doing() 
+{
+    task=$(echo -n "$1" | choose 1:) 
+    notify-send "working on $task"
+    echo "$task" > $doingfile
 }
 
 
 function delete()
 {
+    task=$(echo -n "$1" | choose 1:)
+    if [[ $(< $doingfile) == $task ]]; then
+        > $doingfile
+    fi
+
 	 echo "$1" | awk '{print $1;}' | tr -d '.' | xargs bash $0 del; display
 }
 
 function dmenu_main()
 {
 	lines=$(wc -l <$todofile | tr -d '\n')
-	option=$(echo -e "Add\nDisplay\n" | dmenu -b -i -p "$lines tasks")
+	option=$(echo -e "Add\nDisplay\n" | dmenu -i -fn "FontAwesome" -l 5 -sb '#6272A4' -sf '#F8F8F2' -nb '#282A36' -nf '#F8F8F2' -p "$lines tasks")
 
 	case $option in 
 		"Add") add;;
